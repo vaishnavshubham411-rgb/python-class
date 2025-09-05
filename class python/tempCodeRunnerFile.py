0fe8f0643dc7b375c35a2f63f1415d9e5748fcb6
vaishnavshubham411-rgb <vaishnavshubham411@gmail.com>
2025-09-05 18:07:50 +0530

    Update tempCodeRunnerFile.py

--- class python/tempCodeRunnerFile.py
+++ class python/tempCodeRunnerFile.py
@@ -1,5 +1,6 @@
 
-# list1=[1,2,3,4,5]
-# list1.replace[3][]#replace is use for update specifiv value on certain index in list 
-# print(list1)
+list1=[1,2,3,4,5]
+list1.replace[3][]#replace is use for update specifiv value on certain index in list 
+print(list1)
+
 
